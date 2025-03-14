44750cec60f187245282842600eab48a03ef764b
Pulkit Kumar Mathur <pulkitkumarmathur1@gmail.com>
2025-03-15 00:39:15 +0530

    feat: Add solution for Maximum Candies Allocated to K Children problem

---
+++ 2226.maximum-candies-allocated-to-k-children.cpp
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode id=2226 lang=cpp
+ *
+ * [2226] Maximum Candies Allocated to K Children
+ */
+
+// @lc code=start
+class Solution {
+    long long func(vector<int>& arr, int mid, long long k) {
+        long long ans = 0;
+        for (int i = 0; i < arr.size(); i++)
+            if (mid > 0) ans += (arr[i] / mid);
+        return ans >= k;
+    }
+
+public:
+    long long maximumCandies(vector<int>& candies, long long k) {
+        long long l = 1;
+        long long h = *max_element(candies.begin(), candies.end());
+        while (l <= h) {
+            long long mid = (h + l) / 2;
+            if (func(candies, mid, k)) {
+                l = mid + 1;
+            } else {
+                h = mid - 1;
+            }
+        }
+        return h;
+    }
+};
+// @lc code=end
+
